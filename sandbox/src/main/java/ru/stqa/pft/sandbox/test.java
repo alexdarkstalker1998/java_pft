234c8c77f39341603b6415491c30e0833e3bee11
alexdarkstalker1998 <aleksandr_karchev_pnz@mail.ru>
2022-10-30 20:53:28 +0300

    ДОгнал

--- sandbox/src/main/java/ru/stqa/pft/sandbox/test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/test.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.sandbox;
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        String somebody = "World";
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 6;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 }
